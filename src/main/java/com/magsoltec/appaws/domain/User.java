3fbbd6c4adecefd0548a15e93bcefd0e2dca5f4c
luisNovaes <luismagnovaes@gmail.com>
2020-06-25 09:37:51 -0300

    Add role attribute role

--- src/main/java/com/magsoltec/appaws/domain/User.java
+++ src/main/java/com/magsoltec/appaws/domain/User.java
@@ -3,6 +3,8 @@ package com.magsoltec.appaws.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.magsoltec.appaws.domain.enums.Role;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 
