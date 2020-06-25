d3547d128bce5e1c3b4447d2d4dd13ece5cab2cf
luisNovaes <luismagnovaes@gmail.com>
2020-06-24 21:47:25 -0300

    Initial commit

---
+++ src/main/java/com/magsoltec/appaws/AppawsApplication.java
@@ -0,0 +1,13 @@
+package com.magsoltec.appaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magsoltec/appaws/AppawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magsoltec.appaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
