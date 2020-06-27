5d0f05e35340cb9db1285d6e1a50fcf76a236968
luisNovaes <luismagnovaes@gmail.com>
2020-06-27 18:24:53 -0300

    Create user service

---
+++ src/main/java/com/magsoltec/appaws/service/UserService.java
@@ -0,0 +1,58 @@
+package com.magsoltec.appaws.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magsoltec.appaws.domain.User;
+import com.magsoltec.appaws.repository.UserRepository;
+import com.magsoltec.appaws.util.HashUtil;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User save(User user) {
+
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+
+		User createUser = userRepository.save(user);
+		return createUser;
+
+	}
+
+	public User update(User user) {
+
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+
+		User updateUser = userRepository.save(user);
+		return updateUser;
+
+	}
+
+	public User getById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+
+	public List<User> listAll() {
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+
+	public User login(String email, String password) {
+
+		password = HashUtil.getSecureHash(password);
+
+		Optional<User> result = userRepository.login(email, password);
+		return result.get();
+
+	}
+
+}
