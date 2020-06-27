52a9f541976292019e13cf343073fe57d8492a9c
luisNovaes <luismagnovaes@gmail.com>
2020-06-27 18:51:14 -0300

    Create requestService

---
+++ src/main/java/com/magsoltec/appaws/service/RequestService.java
@@ -0,0 +1,54 @@
+package com.magsoltec.appaws.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magsoltec.appaws.domain.Request;
+import com.magsoltec.appaws.domain.enums.RequestState;
+import com.magsoltec.appaws.repository.RequestRepository;
+
+@Service
+public class RequestService {
+
+	@Autowired
+	private RequestRepository requestRepository;
+
+	public Request save(Request request) {
+
+		request.setState(RequestState.OPEN);
+		request.setCreationDate(new Date());
+
+		Request createRequest = requestRepository.save(request);
+
+		return createRequest;
+	}
+
+	public Request update(Request request) {
+		Request updateRequest = requestRepository.save(request);
+		return updateRequest;
+	}
+
+	public Request getById(Long id) {
+
+		Optional<Request> result = requestRepository.findById(id);
+		return result.get();
+
+	}
+
+	public List<Request> listAll() {
+		List<Request> requests = requestRepository.findAll();
+		return requests;
+
+	}
+
+	public List<Request> listAllByOwnerId(Long ownerId) {
+		List<Request> requests = requestRepository.findAllByOwnerId(ownerId);
+		return requests;
+
+	}
+
+}
