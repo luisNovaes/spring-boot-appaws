108844e23b38e5df322c648051c875808514fff1
luisNovaes <luismagnovaes@gmail.com>
2020-06-27 18:25:09 -0300

    create hash util

---
+++ src/main/java/com/magsoltec/appaws/util/HashUtil.java
@@ -0,0 +1,11 @@
+package com.magsoltec.appaws.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class HashUtil {
+
+	public static String getSecureHash(String text) {
+		String hash = DigestUtils.sha256Hex(text);
+		return hash;
+	}
+}
