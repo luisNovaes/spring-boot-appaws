a64bfa9df647037e7880af66557190a5eae2db82
luisNovaes <luismagnovaes@gmail.com>
2020-06-28 11:25:50 -0300

    Create RequestStageResource

---
+++ src/main/java/com/magsoltec/appaws/resource/RequesteStageResource.java
@@ -0,0 +1,37 @@
+package com.magsoltec.appaws.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.magsoltec.appaws.domain.RequestStage;
+import com.magsoltec.appaws.service.RequestStageService;
+
+@RestController
+@RequestMapping(value = "request-stages")
+public class RequesteStageResource {
+
+	@Autowired
+	private RequestStageService requestStageService;
+
+	@GetMapping
+	public ResponseEntity<RequestStage> save(@RequestBody RequestStage requestStage) {
+		RequestStage createRequestStage = requestStageService.save(requestStage);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createRequestStage);
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<RequestStage> getById(@PathVariable(name = "id") Long id) {
+		RequestStage stage = requestStageService.getById(id);
+		return ResponseEntity.ok(stage);
+	}
+	
+	
+
+}
