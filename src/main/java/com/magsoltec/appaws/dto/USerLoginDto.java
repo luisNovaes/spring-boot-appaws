20cab3267d28a3b8d522506bf70742b3e3921220
luisNovaes <luismagnovaes@gmail.com>
2020-06-28 10:23:54 -0300

    Cerate UserLoginDto Class

---
+++ src/main/java/com/magsoltec/appaws/dto/USerLoginDto.java
@@ -0,0 +1,17 @@
+package com.magsoltec.appaws.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class USerLoginDto {
+
+	private String email;
+	private String password;
+
+}
