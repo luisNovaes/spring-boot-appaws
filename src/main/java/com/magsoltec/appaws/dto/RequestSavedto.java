c0c31bf53abdb9aac155556d31714575d5401661
luisNovaes <luismagnovaes@gmail.com>
2020-06-30 19:33:50 -0300

    Create RequestSavedto

---
+++ src/main/java/com/magsoltec/appaws/dto/RequestSavedto.java
@@ -0,0 +1,40 @@
+package com.magsoltec.appaws.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.magsoltec.appaws.domain.Request;
+import com.magsoltec.appaws.domain.RequestStage;
+import com.magsoltec.appaws.domain.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RequestSavedto {
+
+	@NotBlank(message = "Subject required")
+	private String subject;
+
+	private String description;
+
+	@NotNull(message = "Owner required")
+	private User owner;
+
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
+
+	public Request transformToRequest() {
+		Request request = new Request(null, this.subject, this.description, null, null, this.owner, this.stages);
+		return request;
+
+	}
+
+}
