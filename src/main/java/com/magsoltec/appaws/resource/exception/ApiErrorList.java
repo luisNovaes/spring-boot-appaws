5d26444f9166d03cf1ed735158bba7aa9e404027
luisNovaes <luismagnovaes@gmail.com>
2020-06-30 20:17:32 -0300

    Create  ApiErrorList

---
+++ src/main/java/com/magsoltec/appaws/resource/exception/ApiErrorList.java
@@ -0,0 +1,23 @@
+package com.magsoltec.appaws.resource.exception;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiErrorList extends ApiError {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<String> errors;
+
+	public ApiErrorList(int code, String msg, Date date, List<String> errors) {
+		super(code, msg, date);
+		this.errors = errors;
+
+	}
+
+}
