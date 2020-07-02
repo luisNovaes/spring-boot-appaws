abeadf21cdbeac077c73bdc15dac2411122de208
luisNovaes <luismagnovaes@gmail.com>
2020-07-02 15:25:46 -0300

    Config spring security 4 JWT

--- src/main/java/com/magsoltec/appaws/security/SecurityConfig.java
+++ src/main/java/com/magsoltec/appaws/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.magsoltec.appaws.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -27,10 +29,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
+
+	}
+
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 
 		return super.authenticationManagerBean();
 	}
+
 }
